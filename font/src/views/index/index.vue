2357aaee16edff4aec4c2c8d231740149b8c0c4c
郑森义 <2442827802@qq.com>
2020-05-30 19:48:46 +0800

    重新初始化前端项目

---
+++ font/src/views/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
